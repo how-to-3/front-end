758647a262018af6168313d09d9756c20c0976bc
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-03-02 15:21:20 -0800

    Creator Form created

--- src/components/CreatorForm.js
+++ src/components/CreatorForm.js
@@ -21,11 +21,65 @@
         <BUTTON>SUBMIT NEW HOW-TO!</BUTTON>
 </FORM> */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-const CreatorForm = () => {
+const CreatorForm = props => {
+// setting State for Creator Card     
+// const [Creator, setCreator] = useState({
+//         howToTitle: "",
+//         howToDescription: "",
+//         howToAuthor: "",
+// })
+
+// //onChange handler
+// const handleChanges = e => {
+//     setCreator({...Creator,[e.target.name]: e.target.value})
+// }
+
+// //handleSubmit 
+// const handleSubmit = e => {
+//     e.preventDefault();
+//     props.setCreator([Creator])
+// }
+const [isOpen, setIsOpen] = useState(true);
+const toggle = () => setIsOpen(!isOpen);
+
+
         return (
-                <div>Creator Form</div>
+            <div className="colapse-container" style={{width:"100%", height:"100%"}}>
+            <div style={{ padding:"2% 30% 0 30%", display:"flex", flexDirection:"column", justifyContent:"center", backgroundColor:"#3e444a"}}>
+        <h2 style={{display:"flex", justifyContent:"center", margin: "0 0 5% 1%", color:"white"}}>Create a How-to!</h2>
+        <Button color="dark" onClick={toggle} style={{ marginBottom: '1rem' }}>add a how-to </Button>
+        <Collapse isOpen={isOpen}>
+            <div style={{}}>
+            <Card color="dark">
+            <div style={{}}>
+                <CardBody color="dark">
+                    <div style={{margin:"0 20% 0 20%"}}>
+            <form style={{display:"flex", flexDirection:"column"}}>
+                <label style={{color:"White",}}>Title</label>
+                <input
+              
+                />
+                <label style={{color:"White",}}>Description </label>
+                <input
+                
+                />
+                    <label style={{color:"White",}}>Author Name</label>
+                <input
+                
+                />
+                <button style={{margin:"10% 0 0 0", borderRadius:"5px", width:"100%",}}>Publish!</button>
+            </form>
+            </div>
+                </CardBody>
+                </div>
+            </Card>
+            </div>
+        </Collapse>
+    </div>
+</div>
         )
 }
 
